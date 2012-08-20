8faa5188ac301a69c48be86398fc0c6909f066d0
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-08-20 18:09:53 +0200

    forgot to remove old file, fix rar archiver

--- compression/rar_archive.py
+++ compression/rar_archive.py
@@ -8,9 +8,22 @@ class RarArchive:
         self.files = files
         self.out_path = "{0}.{1}".format(out_path, 'rar')
 
-        self.archive = archive.Archive(self.out_path, '.')
-        for file in self.files:
-            self.archive.add_file(file)
-
     def compress(self):
+        tempfile = open('tempfile', 'w')
+        tempfile.write(self._compile_files())
+        tempfile.close()
+
+        self.archive = archive.Archive(self.out_path, '.')
+        self.archive.add_file('tempfile')
         self.archive.run()
+
+    def _compile_files(self):
+        contents = []
+        for file in self.files:
+            contents.append(self._read_all(file))
+        return "\n".join(contents)
+
+    def _read_all(self, filepath):
+        print filepath
+        with open(filepath) as file:
+            return file.read()
