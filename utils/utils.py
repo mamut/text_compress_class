4749045da7203ba6258eeea6bf6fbca221f1143e
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-08-20 14:08:29 +0200

    use list, not a generator

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import os
 
 def files_in_dir(dirpath):
-    return (os.path.join(dirpath, file) for file in os.listdir(dirpath))
+    return [os.path.join(dirpath, file) for file in os.listdir(dirpath)]
 
 def temp_archive_filename(testfile, dataset):
     base = testfile.split('.')[0]
